de0765310d0f76957838d76a986d05e9fb1a589d
Abbos Ismailov <abbosismailov0017@gmail.com>
2023-03-11 15:25:57 +0500

    added about company

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const sun=document.querySelector('.sun');
-const moon=document.querySelector('.moon');
-sun.addEventListener('click',()=>{
+const sun = document.querySelector('.sun');
+const moon = document.querySelector('.moon');
+sun.addEventListener('click', () => {
     document.querySelector('.darcmode').classList.add('sundark')
     sun.classList.add('activ_darcmode')
-moon.classList.remove('activ_darcmode')
-document.body.classList.add('lighted');
+    moon.classList.remove('activ_darcmode')
+    document.body.classList.add('lighted');
 })
-moon.addEventListener('click',()=>{
+moon.addEventListener('click', () => {
     document.querySelector('.darcmode').classList.remove('sundark')
     moon.classList.add('activ_darcmode')
-sun.classList.remove('activ_darcmode')
-document.body.classList.remove('lighted');
+    sun.classList.remove('activ_darcmode')
+    document.body.classList.remove('lighted');
 })
